c6df341283a0bfd7bfbb5e834f7ed8a5b238cfad
onestopstudio <nparcha89@gmail.com>
2022-05-07 11:59:35 +0530

    initial commit

---
+++ app/mylibrary/src/main/java/com/onestopstudio/mylibrary/Point.java
@@ -0,0 +1,20 @@
+package com.onestopstudio.mylibrary;
+
+public class Point {
+
+    float mLatitude;
+    float mLongitude;
+
+    public Point(float latitude, float longitude) {
+        mLatitude = latitude;
+        mLongitude = longitude;
+    }
+
+    public float getLatitude() {
+        return mLatitude;
+    }
+
+    public float getLongitude() {
+        return mLongitude;
+    }
+}
